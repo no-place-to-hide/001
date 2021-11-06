0db82230d4b4087559e54dde4d7fb47c0db2a0be
diakreado <diakreado@gmail.com>
2021-11-06 16:03:51 +0300

    add footer number

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -5,27 +5,32 @@ import './Background.css';
 export const Background = () => {
   return (
     <div className="Background">
-      <h1>
-        Archive Search Results
-      </h1>
-      <p>
-        Results from Exact search to dddskdsksk.com
-      </p>
-      <p>
-        File mine.exe
-      </p>
-      <div className="Background-radio">
-        <div>
-          <input type="radio" name="Size: 111993999 bytes" />
-          Size: 111993999 bytes
+      <div className="Background-body">
+        <h1>
+          Archive Search Results
+        </h1>
+        <p>
+          Results from Exact search to dddskdsksk.com
+        </p>
+        <p>
+          File mine.exe
+        </p>
+        <div className="Background-radio">
+          <div>
+            <input type="radio" name="Size: 111993999 bytes" />
+            Size: 111993999 bytes
+          </div>
+          <div>
+            <input type="radio" />
+            Last changed 1996 Feb 22
+          </div>
+          <div>
+            <input type="radio" name="Size: 111993999 bytes" />
+            Location:
+          </div>
         </div>
-        <div>
-          <input type="radio" />
-          Last changed 1996 Feb 22
-        </div>
-        <div>
-          <input type="radio" name="Size: 111993999 bytes" />
-          Location:
+        <div className="Background-footer">
+          111.10303.1010
         </div>
       </div>
     </div>
---
+++ src/components/Form_02.jsx
@@ -0,0 +1,27 @@
+import './Form_02.css';
+
+export const Form_02 = () => {
+  return (
+    <div className="Form_02">
+      <div className="Form_02-img-container">
+        <img className="Form_02-img" src="./002.png" alt="" />
+      </div>
+      <div className="Form_02-center-text">
+        <div className="Form_02-center-text-item">
+          Pogoda snaruzhi horoshaya.
+        </div>
+      </div>
+      <div className="Form_02-answers">
+        <div className="Form_02-answers-item">
+          Da
+        </div>
+        <div className="Form_02-answers-item">
+          Net
+        </div>
+        <div className="Form_02-answers-item">
+          Ne znayu
+        </div>
+      </div>
+    </div>
+  );
+};
